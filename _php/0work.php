7556cbb9371aa0b0eef9b39fca545b9fbb3e453f
Wynne Chan <wynnechan@ymail.com>
2010-11-26 17:35:51 -0500

    add content

---
+++ _php/0work.php
@@ -0,0 +1,23 @@
+<div class="slide alpha30">
+  <div class="proj-title">Logos</div>
+  <img src="_content/logo1.png" width="845" height="262" alt="Grapevine">
+</div>
+<div class="slide alpha30">
+  <img src="_content/logo2.png" width="845" height="262" alt="Lingo Mojo">
+</div>
+<div class="slide alpha30">
+  <img src="_content/logo3.png" width="845" height="262" alt="Hot Gizmo">
+</div>
+<div class="slide alpha30">
+  <img src="_content/logo4.png" width="845" height="262" alt="L.E.S Sushi">
+</div>
+<div class="slide alpha30">
+  <img src="_content/logo5.png" width="845" height="262" alt="Nutrid">
+</div>
+<div class="slide alpha30">
+  <img src="_content/logo6.png" width="845" height="262" alt="Songkick">
+</div>
+<div class="slide alpha30">
+  <img src="_content/logo7.png" width="845" height="262" alt="Whole Nutrition">
+</div>
+

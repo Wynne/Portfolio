a5278c6e55af1ff30871b3440d3b36297d5b73df
wynne <wynne@monster>
2011-01-10 00:37:50 -0500

    new img

---
+++ index2.php
@@ -0,0 +1,23 @@
+<?php include ('header.php'); ?>
+<body>
+<div class="wrapper">
+	<div class="menu">links</div>
+<div class="content" id="container">	
+	<div id="top">
+		<ul class="intro">
+			<li><h5>Grapevine</h5><p></p></li>
+			<li><h5>Tracks.io</h5><p></p></li>
+			<li><h5>S.H.I</h5><p></p></li>
+			<li><h5>Tokyo Underground</h5><p></p></li>
+		</ul>
+	</div>
+	<ul class="sections">	
+		<li><strong>header 1</strong><ul><li><img src="_img/moodboard.jpg" width="960" height="968" alt="Moodboard"></li></ul></li>
+		<li><strong>header 2</strong><ul><li><img src="_img/process.jpg" width="960" height="994" alt="Process"></li></ul></li>
+		<li><strong>header 5</strong><ul><li><img src="_img/byproducts.jpg" width="960" height="990" alt="Byproducts"></li></ul></li>
+		<li><strong>header 4</strong><ul><li><img src="_img/grapevine.png" width="960" height="1282" alt="Grapevine"></li></ul></li>
+	</ul>
+</div>
+</div>
+</body>
+</html>

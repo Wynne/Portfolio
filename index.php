781a46c49483e864c7f91682e7a3beaa476e19f7
Wynne Chan <wynnechan@ymail.com>
2011-01-15 00:27:55 -0500

    delete unwanted images

--- index.php
+++ index.php
@@ -34,6 +34,7 @@
 			<li><h2>Tracks | in progress</h2>
 			  <ul>
 			    <li><img src="_img/tracks-process2.jpg" width="945" height="1071" alt="Tracks Process"></li>
+			    <li><img src="_img/tracks-iphone.jpg" width="945" height="1514" alt="Tracks iPhone"></li>
 			  </ul>
 			</li>
 		</ul>
